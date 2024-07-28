30e00244d2a380746c49daa6422995e88cdf7ab5
Rabindra Mishra <131529201+RabindraMishra-AIDS@users.noreply.github.com>
2024-07-29 02:06:50 +0530

    Operation on operand in JS

---
+++ Operations.js
@@ -0,0 +1,12 @@
+//************************************Operations************************************************************** */
+let str1="Rabindra"
+let str2=" Mishra"
+let str3=str1+str2
+console.log(str3)
+console.log("1"+2+2);
+console.log("1"+"2");
+console.log(2+2+"3")
+console.log(true);
+console.log(+true)
+console.log(+"");
+
