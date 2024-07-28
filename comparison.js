8a3e62418db677b7c1e3f8c8697a7e0c3c13adb6
Rabindra Mishra <131529201+RabindraMishra-AIDS@users.noreply.github.com>
2024-07-29 02:03:34 +0530

    comparison of datatype

---
+++ comparison.js
@@ -0,0 +1,10 @@
+// Comparison of same datatypes
+console.log(2>1)
+console.log(2==2)
+console.log(56<=589)
+console.log(45<40)
+ // Problematic comparisions ie. All these are unpredictable
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+console.log(undefined==0)
