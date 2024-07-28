aeb49c32bbc7b35a06841051670d7c28f09e540c
Rabindra Mishra <131529201+RabindraMishra-AIDS@users.noreply.github.com>
2024-07-29 02:04:37 +0530

    converting DataTypes

---
+++ TypeConversion.js
@@ -0,0 +1,14 @@
+let score='33'
+console.log(typeof score);
+console.log(typeof (score));
+let valueinNumber = Number(score)
+console.log(typeof valueinNumber);
+console.log(valueinNumber);
+// Case study two
+let score1='33ab'
+console.log(typeof score);
+console.log(typeof (score));
+let valueinnumber1=Number(score1)
+console.log(typeof valueinnumber1);
+console.log(valueinnumber1);
+ 
