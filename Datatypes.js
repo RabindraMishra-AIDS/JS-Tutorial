9015483a301a8c0e00a3265e06cbd8d1fe62cef3
Rabindra Mishra <131529201+RabindraMishra-AIDS@users.noreply.github.com>
2024-07-29 02:05:01 +0530

    Data types in Js

---
+++ Datatypes.js
@@ -0,0 +1,21 @@
+"use strict";      // Treat all codes as newer version
+//alert("Hello")   we are using nodejs , not browser.
+console.log(3 + 3); console.log("Rabindra") 
+//ecma script for standards 'tc39.es'
+const name="Rabindra"  // String
+let age=45             //int 
+let isLearningJavascript=true  // Haa ya Na ie.Boolean
+let stat;             // Khali hai no value assigned
+let state = null;     // Khali nhi hai contains null
+
+//number 
+//string
+//bigint
+//null => Standalone value
+//undefined => no value is assigned 
+//symbol => unique
+
+
+// Maharati (Object)
+console.log(typeof state);
+console.log(typeof undefined);
