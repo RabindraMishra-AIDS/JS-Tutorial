44a37d38085135fafb57b04ce66b9f67720ce103
Rabindra Mishra <131529201+RabindraMishra-AIDS@users.noreply.github.com>
2024-07-29 02:05:34 +0530

    introduction to Javascript

---
+++ Intro.js
@@ -0,0 +1,10 @@
+const accountId=1356
+let user_name="Rabindra"
+let rollno;
+user_name1="Himanshu"
+rollno=23
+rollno=18
+/*
+Prefer not to use var becasue of its scope problem 
+*/
+console.table([accountId,user_name,rollno,user_name1]);
