186644bdb18038c4542216e98efe91a0d106c811
Rabindra Mishra <131529201+RabindraMishra-AIDS@users.noreply.github.com>
2024-07-29 02:07:50 +0530

    Prefix and Suffix operators

---
+++ Prefix Suffix.js
@@ -0,0 +1,8 @@
+
+/* ----------------------- Understanding Prefix and Postfix---------------------------------------------------*/ 
+
+let x=3
+let z=3
+const y=x++;
+const a=++z;
+console.log(x,y,z,a);
