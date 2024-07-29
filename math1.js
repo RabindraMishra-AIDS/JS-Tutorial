aa6f7469bb7e257132712c0ce73cabe16f36b33d
Rabindra Mishra <131529201+RabindraMishra-AIDS@users.noreply.github.com>
2024-07-29 23:21:58 +0530

    Understanding math of JS: Part 1

---
+++ math1.js
@@ -0,0 +1,24 @@
+//+++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++++++++++++++
+// Ye apne aap mai ek object hai.This automatically comes with Javascript eg:
+console.log(Math);
+
+//abs function ie. similiar to modulus function of maths 
+console.log(Math.abs(-23));
+
+//Round of Function 
+console.log(Math.round(4.6));
+
+// To select top value we can use ceil() Function 
+console.log(Math.ceil(4.3));   //5
+
+// To select lower value we can use floor function.
+console.log(Math.floor(4.9));    //4 
+
+//to find power 
+console.log(Math.pow(5,3));   // 5 ka cube ie.125
+
+//To find minimum number in an array min() Function 
+console.log(Math.min(23,3,19,30));
+
+// Similarly we have max()
+console.log(Math.max(4,45,67,));
